2702d6717b5768b47d1f5a81122a62281e4457b5
vartika <dhochakvartika@gmail.com>
2026-05-09 12:07:37 +0530

    Fixed render deployment runtime

--- server/server.js
+++ server/server.js
@@ -11,12 +11,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
+const startServer = async () => {
+  try {
+
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log('MongoDB Connected');
 
@@ -24,5 +30,9 @@ mongoose.connect(process.env.MONGO_URI)
       console.log(`Server running on port ${PORT}`);
     });
 
-  })
-  .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
