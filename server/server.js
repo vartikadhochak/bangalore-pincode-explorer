5fa5f188a0f96338dd2d3d082895bd8915aac71a
vartika <dhochakvartika@gmail.com>
2026-05-09 11:47:40 +0530

    Fixed Render deployment port issue

--- server/server.js
+++ server/server.js
@@ -13,12 +13,16 @@ app.use(express.json());
 
 app.use('/api', routes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
+
     console.log('MongoDB Connected');
 
-    app.listen(5000, () => {
-              console.log('Server running on port 5000');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
